b878a646b8c151da76f9923894ea840270157ec9
hel1yeah <hel1_yeah@ukr.net>
2020-11-04 00:51:30 +0200

    1 lesson

---
+++ budget-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <budget-list :list="list" />
+  </div>
+</template>
+
+<script>
+import BudgetList from "@/components/BudgetList";
+
+export default {
+  name: "App",
+  components: {
+    BudgetList,
+  },
+  data: () => ({
+    list: {
+      1: {
+        type: "INCOME",
+        value: 100,
+        comment: "Some comment",
+        id: 1,
+      },
+      2: {
+        type: "OUTCOME",
+        value: -50,
+        comment: "Some outcome comment",
+        id: 2,
+      },
+    },
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ budget-app/src/components/BudgetList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="budget-list-wrapp">
+    <el-card :header="header">
+      <template v-if="!isEmpty">
+        <div class="list-item" v-for="(item, prop) in list" :key="prop">
+          <span class="budget-comment">{{ item.comment }}</span>
+          <span class="budget-value">{{ item.value }}</span>
+          <el-button type="danger" size="small"> Default </el-button>
+        </div>
+      </template>
+      <el-alert v-else type="info" :closable="false" :title="emptyTitle"/>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetList",
+
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    header: "Budget List",
+    emptyTitle: 'Empty List'
+  }),
+  computed: {
+    isEmpty(){
+      return !Object.keys(this.list).length;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.budget-list-wrapp {
+  max-width: 500px;
+  margin: auto;
+}
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+.budget-value {
+  font-weight: bold;
+  margin: 0 20px 0 auto;
+}
+</style>
